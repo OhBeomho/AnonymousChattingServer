12d2bad8cf06f713ddc94f121748cc1783a3da4e
OhBeomho <tyler0322@naver.com>
2022-07-22 09:20:11 +0900

    abcd

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const express = require("express"),
     dayjs = require("dayjs")
 
 const app = express()
-app.set("port", 5000)
+app.set("port", process.env.PORT || 5000)
 
 const server = http.createServer(app).listen(app.get("port"), () => {
     console.log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] SERVER STARTED. PORT: ${app.get("port")}`)
